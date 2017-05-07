c56fac706d9f3c7362e757840351a93cd364c104
atg.abhishek <atg.abhishek@gmail.com>
2017-05-07 15:52:31 -0400

    test code and gitignore

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask 
+import requests
+import simplejson as json 
 
 app = Flask(__name__)
+url = ""
+with open("keys.json") as infile:
+    url = json.load(infile)['url']
 
 @app.route('/')
 def hello():
+    
+    r = requests.post(url, json={"text" : "i'm being built"} )
     return "hello"
 
 if __name__ == "__main__":
